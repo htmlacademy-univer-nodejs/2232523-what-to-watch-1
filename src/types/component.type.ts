453bdac13129f3ef914373ba3fff1914a4ef2929
Christmasy <istominachr2002@gmail.com>
2022-10-25 13:09:44 +0500

    fix lint checks

--- src/types/component.type.ts
+++ src/types/component.type.ts
@@ -1,5 +1,5 @@
 export const Component = {
-    Application: Symbol.for('Application'),
-    LoggerInterface: Symbol.for('LoggerInterface'),
-    ConfigInterface: Symbol.for('ConfigInterface'),
-  };
+  Application: Symbol.for('Application'),
+  LoggerInterface: Symbol.for('LoggerInterface'),
+  ConfigInterface: Symbol.for('ConfigInterface'),
+};
