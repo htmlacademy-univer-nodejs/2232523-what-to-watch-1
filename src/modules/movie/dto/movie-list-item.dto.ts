9fe054de25025475079c95f2ad17211276b67954
Christmasy <istominachr2002@gmail.com>
2022-12-15 20:27:27 +0500

    eslint fix

--- src/modules/movie/dto/movie-list-item.dto.ts
+++ src/modules/movie/dto/movie-list-item.dto.ts
@@ -2,22 +2,22 @@ import {Expose} from 'class-transformer';
 import { Genre } from '../../../types/genre.type';
 
 export default class MovieListItemDto {
-  
+
   @Expose()
   public title!: string;
-  
+
   @Expose()
   public publicationDate!: Date;
-  
+
   @Expose()
   public genre!: Genre;
-  
+
   @Expose()
   public videoPreviewUri!: string;
-  
+
   @Expose()
   public userId!: string;
-  
+
   @Expose()
   public posterUri!: string;
 
